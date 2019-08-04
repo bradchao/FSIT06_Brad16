831624935ff52d10aebb9b52bc7486312783f0ad
Brad <brad@brad.tw>
2019-08-04 14:02:01 +0800

    2

--- app/src/main/java/tw/org/iii/appps/brad16/MainActivity.java
+++ app/src/main/java/tw/org/iii/appps/brad16/MainActivity.java
@@ -1,11 +1,17 @@
 package tw.org.iii.appps.brad16;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -20,11 +26,14 @@ public class MainActivity extends AppCompatActivity {
     private String[] from = {"title", "type"};
     private int[] to = {R.id.item_title, R.id.item_type};
     private LinkedList<HashMap<String,String>> data = new LinkedList<>();
+    private UIHandler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        handler = new UIHandler();
 
         listView = findViewById(R.id.listView);
         initListView();
@@ -55,6 +64,7 @@ public class MainActivity extends AppCompatActivity {
                     }
                     reader.close();
                     Log.v("brad", sb.toString());
+                    parseJSONData(sb.toString());
                 }catch (Exception e){
                     Log.v("brad", e.toString());
                 }
@@ -62,9 +72,40 @@ public class MainActivity extends AppCompatActivity {
         }.start();
     }
 
+    private void parseJSONData(String json){
+        try {
+            data.clear();
+            JSONArray root = new JSONArray(json);
+            for (int i=0; i<root.length(); i++){
+                JSONObject row = root.getJSONObject(i);
 
+                HashMap<String,String> d = new HashMap<>();
+                d.put(from[0], row.getString("Title"));
+                d.put(from[1], row.getString("TravelType")
+                        .replace('\n',' ')
+                        .replace('\r', ' ')
+                        .replace("  ",""));
+                d.put("pic", row.getString("PhotoUrl"));
+                d.put("content", row.getString("Contents").replace('\r',' '));
 
+                data.add(d);
+            }
 
+            handler.sendEmptyMessage(0);
+        }catch (Exception e){
+            Log.v("brad", e.toString());
+        }
+    }
+
+    private class UIHandler extends Handler {
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            super.handleMessage(msg);
+
+            adapter.notifyDataSetChanged();
+
+        }
+    }
 
 
 
